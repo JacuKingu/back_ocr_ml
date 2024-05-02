2998ced7360b30f15d0148f78d158a848c4669e9
Jacu <71521139@continental.edu.pe>
2024-05-02 14:46:27 -0500

    Change(configuracion del puerto)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoute'); // Importa las rutas de userRoutes.js
+const config = require('./config.json');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.json());
 // Usar las rutas definidas en userRoutes.js
 app.use('/api', userRoutes); // Prefijo '/api' para todas las rutas de userRoutes.js
 
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
